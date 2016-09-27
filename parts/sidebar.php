2093ea5892b9730bee54f50cb994be967c036d4e
stsdc <stanislaw.dac@gmail.com>
2016-09-27 19:54:15 +0300

    sidebar other done

--- parts/sidebar.php
+++ parts/sidebar.php
@@ -1,30 +1,29 @@
 <?php global $dm_settings; ?>
 <section class="section section--side">
     <?php dynamic_sidebar( 'Right Sidebar' ); ?>
-    <!-- <div class="half">
-        <div class="position">
-            <h3>Pozycja</h3>
+    <div class="sblockContainer">
+        <div class="sblock">
+        <h3 class="sblock__title">Pozycja</h3>
 
-            <div class="pos-number">
-                <?php echo $dm_settings[ 'position'] ?>
+            <div class="sblock__pos">
+                1
             </div>
-            <div>miejsce</div>
-            <div class="table-link">
-                <a href="<?php echo $dm_settings['tabela'] ?>"><i class="icon-medal medal"></i> Pełna tabela ligowa</a>
+            <span class="sblock__posLabel">miejsce</span>
+            <div class="sblock__table">
+                <a class="link link--nav" href="<?php echo $dm_settings['tabela'] ?>"><i class="icon-medal medal"></i> Pełna tabela ligowa</a>
             </div>
 
-        </div>
     </div>
 
+    <a href="<?php echo $dm_settings['store'] ?>">
 
-    <div class="half">
-        <div class="sklep">
-            <a href="<?php echo $dm_settings['store'] ?>">
-                <p>SKLEP INTERNETOWY</p>
-                <img src="<?php bloginfo('template_directory'); ?>/img/sklep3.png" />
+    <div class="sblock sblock--shop">
+            <a class="link link--footer link--shop" href="<?php echo $dm_settings['store'] ?>">
+                SKLEP <br />INTERNETOWY
             </a>
-        </div>
     </div>
-    <div style="clear:both;"></div> -->
+</a>
+
+    </div>
 
 </section>
