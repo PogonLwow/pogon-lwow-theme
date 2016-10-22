16c8ea1645fd2820cf725277a4d4a62237b2a688
stsdc <stanislaw.dac@gmail.com>
2016-10-22 03:01:19 +0200

    results done???

--- src/js/parts/sportspress.js
+++ src/js/parts/sportspress.js
@@ -1,4 +1,8 @@
-(function () {
+(function() {
     $('.sportspress style').remove();
     $('.gallery-item a').addClass('link');
+
+    $('.sp-team-name').each(function() {
+        $(this).insertAfter($(this).parent().find('img'));
+    });
 })();
