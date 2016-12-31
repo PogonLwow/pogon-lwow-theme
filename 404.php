a3955d5f9fa004826b937d696f412b0ae08cd4a8
stsdc <s.dac@yandex.ru>
2016-12-31 16:51:54 +0200

    fix 404

--- 404.php
+++ 404.php
@@ -1,42 +1,17 @@
- <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<?php get_header(); ?>
 
-<head>
-    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-    <meta name="description" content="<?php echo esc_attr(get_bloginfo('description')); ?>" />
-    <title>
-        <?php wp_title('&laquo;', true, 'right'); ?>
-    </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<?php get_template_part('parts/head'); ?>
 
-    <?php wp_head(); ?>
-
-</head>
-
-<body <?php body_class(); ?>>
-
-<?php get_template_part('template-part', 'head'); ?>
-
-<?php get_template_part('template-part', 'topnav'); ?>
 
 <?php
-$imagesDir = get_template_directory_uri() . '/img/404/';
+$imagesDir = get_template_directory_uri() . 'build/img/404/';
 $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 $randomImage = $images[array_rand($images) ]; // See comments
 ?>
     <!-- start content container -->
-    <div class="row dmbs-content">
-        <div class="col-md-10 er404-container">
-        <div class="er404" id="er404" style="background-image: url( <?php echo get_template_directory_uri() . '/img/404/' . rand(1,4);?>.jpg)">
-            <div class="tajoj">
-                <div class="e404">404</div>
-                                Ta joj !
-            </div>
-        </div>
-        </div>
-             <?php get_sidebar('right'); ?>
-<!--              <div style="clear: both;"></div>-->
+    <div class="container">
+        <h1 class="single__title">404 Ta joj!</h1>
+    <?php get_template_part('parts/sidebar'); ?>
     </div>
 
     <!-- end content container -->
