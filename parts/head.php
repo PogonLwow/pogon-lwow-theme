4d4cec6c1f9615474532c7fce55cf4c899d3c92c
stsdc <stanislaw.dac@gmail.com>
2020-09-24 21:15:58 +0200

    rm build folder

--- parts/head.php
+++ parts/head.php
@@ -3,11 +3,11 @@
 
 <div class="container head">
     <div class="logoContainer">
-        <a itemprop="url" class="logoContainer__logo" title="<?php bloginfo('name'); ?>" href="<?php echo site_url(); ?>" aria-label="Logo Pogoń Lwów" role="presentation"></a>
+        <a itemprop="url" class="logoContainer__logo logoContainer__logo--klub" title="<?php bloginfo('name'); ?>" href="<?php echo site_url(); ?>" aria-label="Logo Pogoń Lwów" role="presentation"></a>
         <a class="link link--logo" href="<?php echo site_url(); ?>">
             <h1 itemprop="name" class="logoContainer__name"><?php bloginfo('name'); ?></h1>
         </a>
-            <span class="logoContainer__desc">Oficjalna strona klubu</span>
+            <span class="logoContainer__desc"><?php bloginfo('description'); ?></span>
     </div>
     <div id="nav-trigger" class="head__trigger">
         <div class="navicon-button x">
