65d3fdb09fd59ae3427914ede62d96d28241a067
stsdc <stanislaw.dac@gmail.com>
2020-09-30 00:25:36 +0200

    fix title text

--- parts/social-links.php
+++ parts/social-links.php
@@ -1,8 +1,8 @@
 <div class="social">
-            <a class="link link--social link--tw" title="Twitter Pogoni Lwów" href="<?php echo get_theme_mod('twitter-url'); ?>">
+            <a class="link link--social link--tw" title="Nasz profil na Twitterze" href="<?php echo get_theme_mod('twitter-url'); ?>">
                 <i class="icon-twitter social__icon social__icon--tw"></i>
             </a>
-            <a class="link link--social link--fb" title="Fanpage Pogoni Lwów" href="<?php echo get_theme_mod('facebook-url'); ?>">
+            <a class="link link--social link--fb" title="Nasz fanpage" href="<?php echo get_theme_mod('facebook-url'); ?>">
                 <i class="icon-facebook social__icon social__icon--fb"></i>
             </a>
 </div>
